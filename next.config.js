f03c89f998e74c8ada49d96dc09e102c93778b2f
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-11-12 01:20:13 -0400

    remove external

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-const externalReact = require('webpack-external-react');
-
 const nextConfig = {
- /*  webpack: (config) => {
-    config.externals = externalReact.externals
-    return config
-  }, */
+
   experimental: {
     urlImports: ['http://localhost:1337/uploads'],
   },
