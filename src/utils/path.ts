72e3fbba31b0511071ac5ad9a3f94e296dd778b4
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-11-12 01:30:38 -0400

    update path

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,4 +1,4 @@
-export const fallbackRestUrl: string = 'http://localhost:1337'
-const fallbackUrl: string = 'http://localhost:1337/graphql'
+export const fallbackRestUrl: string = 'https://expomorro-dev-api.thecodeworkers.com'
+const fallbackUrl: string = 'https://expomorro-dev-api.thecodeworkers.com/graphql'
 
 export default fallbackUrl
