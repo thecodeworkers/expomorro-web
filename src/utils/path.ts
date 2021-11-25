6fb0182252c2b2a340ad8a75a50a9cb5828e3aff
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-11-25 10:56:00 -0400

    added correct path

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,5 +1,9 @@
-export const fallbackRestUrl: string = 'https://expomorro-dev-api.thecodeworkers.com'
-const fallbackUrl: string = 'https://expomorro-dev-api.thecodeworkers.com/graphql'
+export const fallbackRestUrl: string = 'https://expomorro-stg-api.thecodeworkers.com'
+const fallbackUrl: string = 'https://expomorro-stg-api.thecodeworkers.com/graphql'
+
+
+//export const fallbackRestUrl: string = 'https://expomorro-dev-api.thecodeworkers.com'
+//const fallbackUrl: string = 'https://expomorro-dev-api.thecodeworkers.com/graphql'
 
 //export const fallbackRestUrl: string = 'http://localhost:1337'
 //const fallbackUrl: string = 'http://localhost:1337/graphql'
