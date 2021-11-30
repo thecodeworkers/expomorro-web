7706f747ee5340da72a497833eb29b69d9ff0cd4
free1f <liseth.yggdrasil@gmail.com>
2021-11-30 11:15:51 -0400

    some indent part6

--- src/components/EventImagePreview/index.tsx
+++ src/components/EventImagePreview/index.tsx
@@ -5,28 +5,28 @@ import { fallbackRestUrl } from '@utils/path'
 
 const EventImagePreview: FC<Props> = ({ data }) => {
 
-  const title = data?.title
-  const isVisible = data?.visible
-  const image = data?.image
-  const imageResponsive = data?.responsive
+	const title = data?.title
+	const isVisible = data?.visible
+	const image = data?.image
+	const imageResponsive = data?.responsive
 
-  return (
-    <>
-      {
-        isVisible && 
-        <section className={styles._main}>
-          <div className={styles._boxDescription}>{title}</div>
-            <div className={styles._imageContainer}>
-              <div className={[styles._imageMain, (imageResponsive?.url) ? styles._imageDesktop : ''].join(' ')} style={{backgroundImage: `url(${fallbackRestUrl}${image.url})` }}  />
-              { 
-                imageResponsive?.url && 
-                <div className={[styles._imageMain, styles._imageMobile].join(' ')} style={{backgroundImage: `url(${fallbackRestUrl}${imageResponsive.url})`}} />
-              }
-          </div>
-        </section>
-      }
-    </>
-  )
+	return (
+		<>
+			{
+				isVisible && 
+				<section className={styles._main}>
+					<div className={styles._boxDescription}>{title}</div>
+          <div className={styles._imageContainer}>
+            <div className={[styles._imageMain, (imageResponsive?.url) ? styles._imageDesktop : ''].join(' ')} style={{backgroundImage: `url(${fallbackRestUrl}${image.url})` }}  />
+            { 
+              imageResponsive?.url && 
+              <div className={[styles._imageMain, styles._imageMobile].join(' ')} style={{backgroundImage: `url(${fallbackRestUrl}${imageResponsive.url})`}} />
+            }
+        </div>
+				</section>
+			}
+		</>
+	)
 }
 
 export default EventImagePreview
