b665a3c6ac0268bee47b30d42acddd3500c778b9
free1f <liseth.yggdrasil@gmail.com>
2021-11-30 11:03:35 -0400

    some indent part2

--- src/components/EventImagePreview/index.tsx
+++ src/components/EventImagePreview/index.tsx
@@ -16,10 +16,10 @@ const EventImagePreview: FC<Props> = ({ data }) => {
         isVisible && 
         <section className={styles._main}>
           <div className={styles._boxDescription}>{title}</div>
-          <div className={styles._imageContainer}>
-          <div className={[styles._imageMain, (imageResponsive?.url) ? styles._imageDesktop : ''].join(' ')} style={{backgroundImage: `url(${fallbackRestUrl}${image.url})` }}  />
-          { imageResponsive?.url && <div className={[styles._imageMain, styles._imageMobile].join(' ')} style={{backgroundImage: `url(${fallbackRestUrl}${imageResponsive.url})`}} />}
-        </div>
+            <div className={styles._imageContainer}>
+            <div className={[styles._imageMain, (imageResponsive?.url) ? styles._imageDesktop : ''].join(' ')} style={{backgroundImage: `url(${fallbackRestUrl}${image.url})` }}  />
+            { imageResponsive?.url && <div className={[styles._imageMain, styles._imageMobile].join(' ')} style={{backgroundImage: `url(${fallbackRestUrl}${imageResponsive.url})`}} />}
+          </div>
         </section>
       }
     </>
