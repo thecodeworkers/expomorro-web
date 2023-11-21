609996eaf4f232183b9f0dc7d5b2899b25681251
Jorge Bastidas <jorgebastidas9@gmail.com>
2023-11-21 11:35:18 -0400

    update path

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,6 +1,5 @@
-export const fallbackRestUrl: string = 'https://expomorro-dev-api.thecodeworkers.com'
-const fallbackUrl: string = 'https://expomorro-dev-api.thecodeworkers.com/graphql'
-
+export const fallbackRestUrl: string = 'https://admin.expomorro.theempire.tech'
+const fallbackUrl: string = 'https://admin.expomorro.theempire.tech/graphql'
 // export const fallbackRestUrl: string = 'http://localhost:1337'
 // const fallbackUrl: string = 'http://localhost:1337/graphql'
 
